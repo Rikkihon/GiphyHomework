a7833deb6fb7366121375f7436a7fc5f6b5e3268
Rikki Honnold <rikki@rikkis-mbp.lan>
2018-03-25 21:29:04 -0500

    adding more things

--- js.js
+++ js.js
@@ -14,8 +14,6 @@ $.ajax({
 
         for (var i = 0; i < topics.length; i++) {
             var giphyURL = response.data[i].images.fixed_height.url;
-            console.log(response);
-            console.log(giphyURL);
             var imgSrc = ('img src=""');
             var giphyRating = response.data[i].rating;
             var giphyButton = $("<button>");
@@ -23,25 +21,31 @@ $.ajax({
             giphyButton.html("<p>"+ topics[i]+"</p>");
             $(".jumbotron").append(giphyButton)
             $("#inputGroup-sizing-default").prepend("Rating: " + giphyRating + " ");
-        }
-    
-
-             for (var i = 0; i < topics.length; i++)
-             $("#inputGroup-sizing-default").prepend("Rating: " + giphyRating + " ");
-             console.log(giphyRating);
-            
+        }     
     
     }
 
 });
+$(".gif").on("click", function() {
+    //http://umn.bootcampcontent.com/University-of-Minnesota-Boot-Camp/MINSTP201802FSF1-Class-Repository-FSF/blob/master/06-ajax/01-Activities/15-PausingGifs/Solved/pausing-gifs-solution.html
+    // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+    var state = $(this).attr("data-state");
+    // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+    // Then, set the image's data-state to animate
+    // Else set src to the data-still value
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
 
       // Part 3 - Clear the Gif using the reset_button id (#)
       $('#reset_button').on('click', function(){
         // Grab the img using the id and change the src to empty to remove the image
         $('#here_is_gif').attr("src",'');
       })
-$(".clearBtn").on("click", function (event) {
-    $('.footer').clear();
 
 });
 });
